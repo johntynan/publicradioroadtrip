3a6c83f97427c247a9425647cb06127451ae8acc
jgtynan <jgtynan@gmail.com>
2010-09-29 08:01:35 +0200

    Attempting to have a many-to-many relationship for stories to roadtrips.  Shoring up the model based on massimo's suggestions.

--- models/db_custom.py
+++ models/db_custom.py
@@ -9,6 +9,14 @@ if sys.path[0] != site_packages_path:
     sys.path.insert(0, site_packages_path)
 
 from gluon.settings import settings
+auth = Auth(globals(),db)
+auth.define_tables()
+crud = Crud(globals(),db)
+
+if auth.is_logged_in(): 
+    me=auth.user_id
+else: 
+    me=None 
 
 # if running on Google App Engine
 if settings.web2py_runtime_gae:
@@ -21,36 +29,28 @@ else:
     # if not, use SQLite or other DB
     db = DAL('sqlite://storage.sqlite')  
 
-if auth.is_logged_in():
-    me=auth.user.id
-else:
-    me=None
-
-db.define_table(
-    'users', 
-    Field('name'), 
-    Field('email')
-    )
 
 db.define_table(
     'roadtrip', 
     Field('name'), 
     Field('description', 'text'),
-    Field('created_by',default=me,writable=False,readable=False),
-    Field('created_on','datetime',default=request.now,writable=False,readable=False)
+    # Field('created_by', db.auth_user, default=auth.user_id, writable=False, readable=False),
+    Field('created_by', default=me, writable=False, readable=False),
+    Field('created_on','datetime',default=request.now, writable=False, readable=False)
     )
                             
 db.define_table(
     'story', 
-    Field('user_id', db.users,readable=False,writable=False),
+    Field('user_id', readable=False,writable=False),
     Field('roadtrip_id', db.roadtrip), 
     Field('story_id', 'integer'),
     Field('title'), 
     Field('latitude'),
     Field('longitude'),
     Field('comment', 'text'),
-    Field('created_by',default=me,writable=False,readable=False),
-    Field('created_on','datetime',default=request.now,writable=False,readable=False)
+    # Field('created_by', db.auth_user, default=auth.user_id, writable=False, readable=False),
+    Field('created_by', default=me, writable=False, readable=False),
+    Field('created_on','datetime', default=request.now, writable=False, readable=False)
     )
     
-db.story.roadtrip_id.requires=IS_IN_DB(db(db.roadtrip.created_by==me),'roadtrip.id','%(name)s')
+# db.story.roadtrip_id.requires=IS_IN_DB(db(db.roadtrip.created_by==auth.user_id),'roadtrip.id','%(name)s', multiple=True) db.story.roadtrip_id.requires=IS_IN_DB(db(db.roadtrip.created_by==auth.user_id),'roadtrip.id','%(name)s', multiple=True) 
