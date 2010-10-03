0cdff32aab4cb196af85792089f5adbd0531ad64
jgtynan <jgtynan@gmail.com>
2010-10-03 10:47:52 +0200

    Still working out that typo.

--- static/getvariables.js
+++ static/getvariables.js
@@ -7,6 +7,6 @@ var story_title = unescape(pairs[1].substring(6))
 document.write("Story ID: " + story_id + "<br />");
 document.write("Story Title: " + story_title + "<br />");
 
-document.story_form.story_story_npr_id.value = story_id
+document.story_form.story_npr_id.value = story_id
 document.story_form.story_story_title.value = story_title
 
